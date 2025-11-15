01453a28869be336a55f20b7b9bc740002e22a5e
Fredy Eleazar Cardona Banegas <fcardonabanegas@gmail.com>
2025-11-15 12:11:03 -0600

    update

--- schemas/Clientes.py
+++ schemas/Clientes.py
@@ -1,17 +1,26 @@
 from pydantic import BaseModel
 from typing import Optional
+from datetime import datetime
 
+
+# Campos que el cliente puede enviar
 class ClienteBase(BaseModel):
     nombre: str
     email: Optional[str] = None
     telefono: Optional[str] = None
-    fecha_registro: Optional[str] = None
 
-class ClienteCreate(ClienteBase): ...
-class ClienteUpdate(ClienteBase): ...
+
+class ClienteCreate(ClienteBase):
+    pass
+
+
+class ClienteUpdate(ClienteBase):
+    pass
+
 
 class ClienteResponse(ClienteBase):
     id: int
-    fecha_registro: Optional[str] = None
+    fecha_registro: Optional[datetime] = None
+
     class Config:
         orm_mode = True
