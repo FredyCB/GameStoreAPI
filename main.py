95c264d1132b9a3c9200e2399c1d41887d511b32
Fredy Eleazar Cardona Banegas <fcardonabanegas@gmail.com>
2025-11-16 18:02:28 -0600

    update main

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from datetime import datetime
 from routes import Clientes, Inventario, Juegos, Orden
@@ -10,3 +11,6 @@ app.include_router(Clientes.router, prefix="/clientes", tags=["Clientes"])
 app.include_router(Inventario.router, prefix="/inventario", tags=["Inventario"])
 app.include_router(Juegos.router, prefix="/juegos", tags=["Juegos"])
 app.include_router(Orden.router, prefix="/ordenes", tags=["Ordenes"])
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
